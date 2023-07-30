011b9c41d7ed9365bc13d72ba0aa70d2672bb4a1
Goncharuk Alexander <a.d.goncharuk@gmail.com>
2023-07-30 13:17:52 +0300

    реализовать функционал создания и удаления фильмов, а так же получение списка всех сохраненных пользователем фильмов

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,11 +1,32 @@
-module.exports.getMovies = () => {
+const Movie = require('../models/movie');
+const NotFoundError = require('../errors/NotFoundError');
+const ForbiddenError = require('../errors/ForbiddenError');
 
-}
+module.exports.getMovies = (req, res, next) => {
+  Movie.find({owner: req.user})
+    .then(movies => res.status(200).send({ data: movies }))
+    .catch(err => {
+    });
+};
 
-module.exports.createMovie = () => {
+module.exports.createMovie = (req, res, next) => {
+  Movie.create({ ...req.body, owner: req.user._id })
+    .then(movie => res.status(201).send({ data: movie }))
+    .catch(err => {
+    });
+};
 
-}
+module.exports.deleteMovie = (req, res, next) => {
+  Movie.findById(req.params.id)
+    .then(movie => {
+      if (!movie) throw new NotFoundError;
 
-module.exports.deleteMovie = () => {
+      if (movie.owner._id.toString() !== req.user._id) throw new ForbiddenError;
 
-}
+      Movie.findByIdAndDelete(req.params.id)
+        .then(movie => res.status(200).send({ data: movie }))
+        .catch(next);
+    })
+    .catch(err => {
+    });
+};
