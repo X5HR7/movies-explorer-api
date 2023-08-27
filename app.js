92cefdad8733a7b6436c30cd2b75168a73beeb87
Goncharuk Alexander <a.d.goncharuk@gmail.com>
2023-08-27 16:51:10 +0300

    refactor: настройки cors

--- app.js
+++ app.js
@@ -15,9 +15,10 @@ const errorHandler = require('./utils/errorHandler');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
-mongoose.connect(DB_URL, {
-  useNewUrlParser: true
-})
+mongoose
+  .connect(DB_URL, {
+    useNewUrlParser: true
+  })
   .then(() => console.log('Connected to DB'))
   .catch(() => console.log('Can not connect to DB'));
 
@@ -30,7 +31,16 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      'http://localhost:3000',
+      'https://movies-explorer.service.nomoreparties.co',
+      'http://movies-explorer.service.nomoreparties.co/'
+    ],
+    credentials: true
+  })
+);
 app.use(requestLogger);
 
 app.use('/', router);
